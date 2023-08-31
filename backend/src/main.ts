cd1876116a8cdfc7d0d387439a370fa7021c2432
can-can-need-bug <772778995@qq.com>
2023-09-01 02:20:02 +0800

    添加接口文档

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app
     .setGlobalPrefix('api')
-    .useStaticAssets(join(__dirname, '../public/'), { prefix: '/static' })
-    .listen(3000);
+    .useStaticAssets(join(__dirname, '../public/'), { prefix: '/static' });
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  app.listen(3000);
 }
 bootstrap();

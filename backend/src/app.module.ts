da7ea84611a148e46a95d5f1c001070776182882
can-can-need-bug <772778995@qq.com>
2023-09-02 13:51:00 +0800

    移除 person 模块

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PersonModule } from './person/person.module';
 import { LiveCycleModule } from './live-cycle/live-cycle.module';
 
 @Module({
-  imports: [PersonModule, LiveCycleModule],
+  imports: [LiveCycleModule],
   controllers: [AppController],
   providers: [AppService],
 })

9cf6a4eb4a88419bc2bd6b8cd7966ba3d31d986d
can-can-need-bug <772778995@qq.com>
2023-10-02 21:56:43 +0800

    修改 api

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -29,7 +29,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['i18n', 'api', 'stores', 'electron', 'i18n', 'localForage'],
+    boot: ['i18n', 'stores', 'electron', 'i18n', 'localForage'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.scss'],
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,12 +1,10 @@
 import _Axios from 'axios'
 import { AxiosStatic } from '@/src/typings/axios'
-import i18n from '@/src/i18n'
-import { Notify } from 'quasar'
 import throwError from '../utils/throwError'
+import { Notify } from 'quasar'
 
-const t = i18n.global.t
 const Axios = _Axios as AxiosStatic
-const api = Axios.create({ baseURL: 'http://10.88.18.164:30000/', timeout: 1000 })
+const api = Axios.create({ baseURL: 'http://127.0.0.1:54088/api' })
 
 export const reqs = ref(0)
 
@@ -18,36 +16,42 @@ api.interceptors.request.use(config => {
 api.interceptors.response.use(
   config => {
     reqs.value--
-    if (config.config.filterResponse === false) return config
+    const msgType = {
+      positive: 'success',
+      error: 'negative',
+      warn: 'warning',
+      info: 'info'
+    }[config.data?.type + '']
+    if (msgType === 'negative') throwError(config.data.msg)
+    if (msgType) {
+      Notify.create({
+        type: msgType,
+        message: config.data.msg
+      })
+    }
     return config.data.data
   },
   err => {
     reqs.value--
-    const errMsg =
-      err.response?.data?.error_msg || err.response?.data?.message || t('request-error')
+    const code = err.code
+    const msg = err.message
+    const errMsg = `${code}：${msg}`
     throwError(errMsg)
     throw new Error(errMsg)
   }
 )
 
-declare module '@/src/typings/axios' {
-  export interface AxiosRequestConfig {
-    /**
-     * 是否过滤响应数据
-     * @default true
-     * @example
-     * api
-     *  .get('/api', { filterResponse: false })
-     *  .then(res => {
-     *    // 如果 filterResponse 为 false 则返回的数据为原始数据
-     *    console.log(res.data.data)
-     *    // 默认情况下返回的数据为过滤后的数据
-     *    console.log(res)
-     *  })
-     */
-    filterResponse?: boolean
-  }
-}
-
 export default api
+
+declare module '@/src/typings/axios' {
+  // export interface AxiosResponse<T = any, D = any> {
+  //   data: T
+  //   status: number
+  //   statusText: string
+  //   headers: RawAxiosResponseHeaders | AxiosResponseHeaders
+  //   config: AxiosRequestConfig<D>
+  //   request?: any
+  // }
+  export type AxiosResponse<T> = T
+}
 
